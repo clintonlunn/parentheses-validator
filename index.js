f75b1d8b769c6a02a3d46e0889c5203a38a87b96
Clinton Lunn <cpl09c@gmail.com>
2019-06-29 13:09:56 -0400

    working lisp validator

--- index.js
+++ index.js
@@ -3,17 +3,58 @@
 // (in Java or JavaScript) which takes in a string as an input and
 // returns true if all the parentheses in the string are properly closed and nested.
 
-
 const sampleLisp = `
-(defun negate (X)
-  "Negate the value of X."  ; This is a documentation string.
-  (- X))  
+    ((lambda (arg) (* arg 2)) 10)
 `
+const openerHandler = (openParen) => {}
 
-function validateLisp (string) {
+const closingHandler = (closingParen) => {
+
+}
+
+function validateLisp(string) {
+    // what we will hold parens in
+    let stack = [];
+    const failMsg = 'invalid lisp';
+    const successMsg = 'valid lisp';
+
     for (i in string) {
-        console.log(string[i]);     
+        // hold the openers
+        // if opener
+        if (string[i] === "(" || string[i] === ")");
+
+        if (string[i] === '(') {
+            stack.push('(')
+        }
+
+        // if closer
+        if (string[i] === ')') {
+            // the previous parentheses should be an opener
+            // so let's look at the previous parenthesis
+
+            const prevParen = stack[stack.length - 1];
+
+            if (prevParen === '(' && stack.length !== 0) {
+                // remove matching opener paren
+                stack.pop();
+
+            } else {
+                return console.log(failMsg);
+
+            }
+        }
+    }
+
+    // then check if stck is empty after the loop
+    if (stack.length === 0) {
+        // then we have matched all of the parentheses
+        return console.log(successMsg);
+
+
+    } else {
+        return console.log(failMsg);
+
     }
 }
 
-validateLisp();
+validateLisp(sampleLisp);

038d98ae13c7b0018fbb2681bab4d74501b1e0bb
Clinton Lunn <cpl09c@gmail.com>
2019-06-30 23:35:14 -0400

    refactor test cases so all pass

--- index.test.js
+++ index.test.js
@@ -14,29 +14,18 @@ test('test a valid lisp', () => {
     expect(validateLisp(example1)).toBe('valid lisp');
 });
 
+test('commenting example', () => {
+    expect(validateLisp(commentingExample)).toBe('valid lisp');
+});
+
 test('test a not valid lisp', () => {
     expect(validateLisp(invalidExample)).not.toBe('valid lisp');
 });
 
 test('test edge case lisp', () => {
-    expect(validateLisp(example1 + '(')).toBe('valid lisp');
+    expect(validateLisp(example1 + '(')).toBe('invalid lisp');
     expect(validateLisp(example1 + ')')).not.toBe('valid lisp');
     expect(validateLisp('(' + example1)).not.toBe('valid lisp');
     expect(validateLisp(')' + example1 + '(')).not.toBe('valid lisp');
     expect(validateLisp('(' + example1 + ')')).toBe('valid lisp');
 });
-
-
-test('test edge case lisp', () => {
-    expect(validateLisp(example1 + '(')).toBe('valid lisp');
-})
-test('test big valid lisp', () => {
-    expect(validateLisp(`
-    (setf x (make-array '(3 3):initial-contents'((1 2 3)(4 5 6)(7 8 9))))
-    (write x)
-  `).toBe('valid lisp'));
-});
-
-// test('test a valid lisp', () => {
-//     expect(validateLisp).toBe(2);
-// });
